3313ca81a2838baa5c50527149fc501c015795d3
JarvisJiang <jiang115jie@gmail.com>
2016-06-21 10:44:14 +0800

    update: 增加循环压缩

--- index.js
+++ index.js
@@ -23,34 +23,66 @@ const file = process.argv[2];
 
 const KEY = 'LNN9yc5v7KNlzQ4RhcD9pPcGhCkmXGtT';
 
-const req = request({
-    url: 'https://api.tinify.com/shrink',
-    method: 'POST',
-    headers: {
-        Authorization: `Basic ${new Buffer(KEY).toString('base64')}`
-    }
-})
-.on('error', err => {
-    console.error(`post image error, ${err.message}`);
-})
-.on('response', res => {
-    console.log(res.statusCode);
-    if(res.statusCode !== 201) {
-        console.warn('error');
-        process.exit(1);
-        return;
-    }
+// 文件大小
+let SIZE;
 
-    const url = res.headers.location;
-    const ws = fs.createWriteStream(file)
-    .on('error', err => {
-        console.error(`get image error, ${err.message}`);
+function createReq() {
+    const req = request({
+        url: 'https://api.tinify.com/shrink',
+        method: 'POST',
+        headers: {
+            Authorization: `Basic ${new Buffer(KEY).toString('base64')}`
+        }
     })
-    .on('close', () => {
-        console.log('success');
+    .on('error', err => {
+        console.error(`post image error, ${err.message}`);
+    })
+    .on('response', res => {
+        console.log(res.statusCode);
+        if(res.statusCode !== 201) {
+            console.warn('error');
+            process.exit(1);
+            return;
+        }
+
+        const url = res.headers.location;
+        const ws = fs.createWriteStream(file)
+        .on('error', err => {
+            console.error(`get image error, ${err.message}`);
+        })
+        .on('close', () => {
+            console.log('success');
+            console.log(`new size is ${SIZE}`);
+            main();
+        });
+
+        request(url).pipe(ws);
     });
 
-    request(url).pipe(ws);
-});
+    return req;
+}
 
-fs.createReadStream(file).pipe(req);
+function main() {
+    fs.stat(file, (err, stats) => {
+        if(err) {
+            console.error(err.message);
+            process.exit(1);
+            return;
+        }
+
+        if(!SIZE) {
+            console.log(`init size is ${stats.size}`);
+        }
+
+        if(SIZE === stats.size) {
+            process.exit(0);
+            return;
+        }
+
+        SIZE = stats.size;
+
+        fs.createReadStream(file).pipe(createReq());
+    });
+}
+
+main();
